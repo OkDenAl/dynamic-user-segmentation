a66fca61ce516ef6315fe3913a5b7488824656cc
Denis <denok629@gmail.com>
2023-08-28 11:08:34 +0300

    impl adding the gistory of operations

--- internal/repository/user_segment/repo.go
+++ internal/repository/user_segment/repo.go
@@ -31,37 +31,84 @@ func New(conn postgres.PgxPool, l logging.Logger) Repository {
 
 func (r *repo) CreateMultSegsForOneUser(ctx context.Context, userId int64, segments []entity.Segment, ttl entity.TTL) error {
 	var (
-		batch = &pgx.Batch{}
-		q     string
+		insertUsersSegmentBatch = &pgx.Batch{}
+		insertUsersSegmentQ     string
+		insertOperationBatch    = &pgx.Batch{}
+		insertOperationQ        = `INSERT INTO operations(user_id,segment_name,type,created_at) VALUES ($1,$2,$3,$4)`
 	)
 	intervalString := fmt.Sprintf("P%dY%dM%dDT%dH%dM%dS", ttl.Years, ttl.Months, ttl.Days, ttl.Hours, ttl.Minutes, ttl.Seconds)
 	if intervalString == "P0Y0M0DT0H0M0S" {
-		q = `INSERT INTO users_segments (user_id,segment_name) VALUES ($1,$2)`
+		insertUsersSegmentQ = `INSERT INTO users_segments (user_id,segment_name) VALUES ($1,$2)`
 	} else {
-		q = fmt.Sprintf("INSERT INTO users_segments (user_id,segment_name,expires_at) VALUES ($1,$2,now()+interval '%s')", intervalString)
+		insertUsersSegmentQ = fmt.Sprintf("INSERT INTO users_segments (user_id,segment_name,expires_at) VALUES ($1,$2,now()+interval '%s')", intervalString)
 	}
-	for _, segment := range segments {
-		batch.Queue(q, userId, segment.Name)
-	}
-	err := r.conn.SendBatch(ctx, batch).Close()
+
+	tx, err := r.conn.Begin(ctx)
 	if err != nil {
-		r.l.Error(fmt.Errorf("user_segment.CreateMultSegsForOneUser - r.conn.SendBatch.Close - %w", err))
+		r.l.Error(fmt.Errorf("user_segment.CreateMultSegsForOneUser - r.conn.Begin - %w", err))
 		return repository.SqlErrorWrapper(err)
 	}
+	defer func() { _ = tx.Rollback(ctx) }()
+
+	for _, segment := range segments {
+		insertUsersSegmentBatch.Queue(insertUsersSegmentQ, userId, segment.Name)
+		insertOperationBatch.Queue(insertOperationQ, userId, segment.Name, entity.AddOperation, time.Now())
+	}
+	err = tx.SendBatch(ctx, insertUsersSegmentBatch).Close()
+	if err != nil {
+		r.l.Error(fmt.Errorf("user_segment.CreateMultSegsForOneUser - r.conn.SendBatch.Close - insertUsersSegmentBatch - %w", err))
+		return repository.SqlErrorWrapper(err)
+	}
+	err = tx.SendBatch(ctx, insertOperationBatch).Close()
+	if err != nil {
+		r.l.Error(fmt.Errorf("user_segment.CreateMultSegsForOneUser - r.conn.SendBatch.Close - insertOperationBatch - %w", err))
+		return repository.SqlErrorWrapper(err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		r.l.Error(fmt.Errorf("user_segment.CreateMultSegsForOneUser - tx.Commit - %w", err))
+		return repository.SqlErrorWrapper(err)
+	}
+
 	return nil
 }
 
 func (r *repo) CreateOneSegForMultUsers(ctx context.Context, userIds []int64, segment entity.Segment) error {
-	batch := &pgx.Batch{}
-	q := `INSERT INTO users_segments (user_id,segment_name) VALUES ($1,$2)`
-	for _, userId := range userIds {
-		batch.Queue(q, userId, segment.Name)
-	}
-	err := r.conn.SendBatch(ctx, batch).Close()
+	var (
+		insertUsersSegmentBatch = &pgx.Batch{}
+		insertUsersSegmentQ     = `INSERT INTO users_segments (user_id,segment_name) VALUES ($1,$2)`
+		insertOperationBatch    = &pgx.Batch{}
+		insertOperationQ        = `INSERT INTO operations(user_id,segment_name,type,created_at) VALUES ($1,$2,$3,$4)`
+	)
+	tx, err := r.conn.Begin(ctx)
 	if err != nil {
-		r.l.Error(fmt.Errorf("user_segment.CreateOneSegForMultUsers - r.conn.SendBatch.Close - %w", err))
+		r.l.Error(fmt.Errorf("user_segment.CreateOneSegForMultUsers - r.conn.Begin - %w", err))
 		return repository.SqlErrorWrapper(err)
 	}
+	defer func() { _ = tx.Rollback(ctx) }()
+
+	for _, userId := range userIds {
+		insertUsersSegmentBatch.Queue(insertUsersSegmentQ, userId, segment.Name)
+		insertOperationBatch.Queue(insertOperationQ, userId, segment.Name, entity.AddOperation, time.Now())
+	}
+	err = tx.SendBatch(ctx, insertUsersSegmentBatch).Close()
+	if err != nil {
+		r.l.Error(fmt.Errorf("user_segment.CreateOneSegForMultUsers - r.conn.SendBatch.Close - insertUsersSegmentBatch  - %w", err))
+		return repository.SqlErrorWrapper(err)
+	}
+	err = tx.SendBatch(ctx, insertOperationBatch).Close()
+	if err != nil {
+		r.l.Error(fmt.Errorf("user_segment.CreateOneSegForMultUsers - r.conn.SendBatch.Close - insertOperationBatch - %w", err))
+		return repository.SqlErrorWrapper(err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		r.l.Error(fmt.Errorf("user_segment.CreateOneSegForMultUsers - tx.Commit - %w", err))
+		return repository.SqlErrorWrapper(err)
+	}
+
 	return nil
 }
 
